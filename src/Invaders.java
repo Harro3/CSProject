fc8e37d27160d7f1d6c9fe994a578a90ac587dc8
Harro3 <quentin.de-capele@epita.fr>
2022-03-23 20:39:55 +0200

    architecture

---
+++ src/Invaders.java
@@ -0,0 +1,6 @@
+public class Invaders {
+    public static void main(String[] args) {
+        StdDraw.setCanvasSize(500, 500);
+        StdDraw.clear(StdDraw.BLACK);
+    }
+}
--- src/Tests.java
+++ src/Tests.java
@@ -1,4 +1,4 @@
-public class Entities {
+public class Tests {
     public static void main(String[] args) {
         StdDraw.filledCircle(0.5, 0.5, 0.5);
     }
